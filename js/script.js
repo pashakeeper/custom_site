af4199eb97d812b40de0f7c045197cff16e516ff
pashakeeper <pashagkjunost@gmail.com>
2023-12-15 16:10:08 +0200

    done

---
+++ js/script.js
@@ -0,0 +1,50 @@
+
+
+
+$(document).ready(function () {
+    AOS.init();
+    if (window.location.pathname === "/") {
+        var typed = new Typed('.main_title', {
+            strings: ['ООО "BLABLABLAH"'],
+            typeSpeed: 60
+        });
+    }
+    $(document).mouseup(function(e) {
+        var container = $(".menu_box");
+        if (!container.is(e.target) && container.has(e.target).length === 0) {
+            $('.menu_box').removeClass('active');
+        }
+    });
+//     burger & mobile menu
+    $('.burger').click(function () {
+        $('.menu_box').addClass('active');
+    })
+    $('.close').click(function () {
+        $('.menu_box').removeClass('active');
+    })
+    $('.galery_holder').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        closeBtnInside: false,
+        fixedContentPos: true,
+        gallery: {
+            enabled: true
+        },
+        mainClass: 'mfp-no-margins mfp-with-zoom',
+        image: {
+            verticalFit: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 400,
+            easing: 'ease-in-out',
+        },
+        callbacks: {
+            open: function() {
+
+            }
+        }
+    });
+
+})
+
